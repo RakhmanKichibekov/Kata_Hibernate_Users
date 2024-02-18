d39937c230a80ab23b36dc66c92c79fcbcb67114
RakhmanKichibekov <kichibekovrahman@yandex.ru>
2024-02-19 00:38:46 +0300

    v1.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,8 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public class Main {
     public static final UserServiceImpl userService = new UserServiceImpl();
